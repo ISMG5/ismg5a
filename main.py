ae65e42c4951241c3a4d453393496769931d4d74
Elias Fernandez <informatica3@imag.com.py>
2026-04-27 14:29:13 -0300

    pg

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask
 import os
+import psycopg2
 
 app = Flask(__name__)
+
+# conexión global (simple)
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 
 @app.route("/")
 def home():
     return "Hola, Railway 🚀"
+
+@app.route("/db")
+def test_db():
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()
+        cur.close()
+        return f"PostgreSQL conectado: {version}"
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
